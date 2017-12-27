23e5ff92df786782db1da79e08d5061443d26f67
Rob Horton <robhorton@outlook.com>
2017-12-26 19:42:27 -0600

    Adding basic unit test structure

---
+++ api/ContactManager.Tests/Controllers/CompanyControllerTest.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContactManager.api;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ContactManager.Tests.Controllers
+{
+    [TestClass]
+    public class CompanyControllerTest
+    {
+        [TestMethod]
+        public void GetCompaniesTest()
+        {
+            // Arrange
+            CompanyController controller = new CompanyController();
+
+            // Act
+            IHttpActionResult result = controller.GetCompanies();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<System.String>));
+            OkNegotiatedContentResult<string> conNegResult = (OkNegotiatedContentResult<string>)result;
+            Assert.IsTrue(string.Empty != conNegResult.Content);
+
+        }
+    }
+}
