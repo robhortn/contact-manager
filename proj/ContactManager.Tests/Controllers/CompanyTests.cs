09919878aae77937a9ef9d36364a12be1705213f
Rob Horton <robhorton@outlook.com>
2018-01-02 11:40:18 -0600

    Adding tests CompaniesByCategoryTest and CompaniesByPhoneTest

--- proj/ContactManager.Tests/Controllers/CompanyTests.cs
+++ proj/ContactManager.Tests/Controllers/CompanyTests.cs
@@ -32,20 +32,45 @@ namespace ContactManager.Tests.Controllers
         public void CompanyByIdTest()
         {
             // Arrange
-            var id = 1;
+            var param = 1;
 
             // Act
-            IHttpActionResult result = controller.CompanyById(id);
+            IHttpActionResult result = controller.CompanyById(param);
 
             // Assert
             Assert.IsNotNull(result);
             //Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<Company>));
         }
 
+        [TestMethod]
+        public void CompaniesByCategoryTest()
+        {
+            // Arrange
+            var param = 1;
 
-        //CompanyByIdTest
-        //CompaniesByCategoryTest
-        //CompaniesByPhoneNumberTest
+            // Act
+            IHttpActionResult result = controller.CompaniesByCategory(param);
+
+            // Assert
+            Assert.IsNotNull(result);
+            //Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<Company>));
+        }
+
+        [TestMethod]
+        public void CompanyByPhoneNumberTest()
+        {
+            // Arrange
+            var param = "3145551211";
+
+            // Act
+            IHttpActionResult result = controller.CompaniesByPhoneNumber(param);
+
+            // Assert
+            Assert.IsNotNull(result);
+            //Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<Company>));
+        }
+
+        
         //CompanyAddTest
         //CompanyUpdateTest
         //CompanyDeleteTest
